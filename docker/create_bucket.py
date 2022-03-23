82fbd1427d63ffd95d88812397e05eef78c0ae29
markgllin <markgllin@hotmail.com>
2022-03-23 17:14:16 -0600

    dockerize script

--- docker/create_bucket.py
+++ docker/create_bucket.py
@@ -4,8 +4,10 @@ s3 = boto3.resource('s3')
 
 bucket_name = "buckety-mcbuckface"
 print(f"Creating bucket with name {bucket_name}!")
-s3.create_bucket(Bucket=bucket_name, 
+s3.create_bucket(
+    Bucket=bucket_name, 
     ACL='private',
-    CreateBucketConfiguration={'LocationConstraint': 'us-west-2'})
+    CreateBucketConfiguration={'LocationConstraint': 'us-west-2'}
+    )
 print(f"Created {bucket_name}!")
 
