61355b69d0164ad54133e99b31ad929bb2994956
markgllin <markgllin@hotmail.com>
2022-03-23 17:44:26 -0600

    missed policy document

--- iam.tf
+++ iam.tf
@@ -1,8 +1,19 @@
+data "aws_iam_policy_document" "ecs_sts_assume_role-document" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
 # for pulling container images and publishing container logs to Cloudwatch
 resource "aws_iam_role" "ecs_task_exec_role" {
   assume_role_policy = data.aws_iam_policy_document.ecs_sts_assume_role-document.json
 
   managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"]
   name                = "ecs_task_exec_role"
-  path                = "/${local.service_name}"
+  path                = "/${local.service_name}/"
 }
